9c03ecbba527c3086ae56551370953dcc374c255
Stephen <68696613+sdw-online@users.noreply.github.com>
2024-09-29 18:53:18 +0100

    Added start and end tasks + updated start_time for demo_dag.py

--- python/short_vids/airflow/demo_1/dags/demo_dag.py
+++ python/short_vids/airflow/demo_1/dags/demo_dag.py
@@ -10,10 +10,17 @@ default_args = {
 
 @dag(default_args=default_args, 
      schedule_interval='*/2 * * * *', 
-     start_date=datetime.now() - timedelta(minutes=1), 
+     start_date=datetime(2024, 9, 28), 
+    #  start_date=datetime.now() - timedelta(minutes=1), 
      tags=['message_display'])
 
-def message_display_dag():
+def message_display_dag():    
+    
+    
+    @task()
+    def display_init_message():
+        print("--- Let's initiate this workflow...✅---") 
+
     
     # Add task for displaying the 1st message 
     @task()
@@ -32,12 +39,18 @@ def message_display_dag():
     def display_third_message():
         print("This is the 3rd message (3/3)🟢")  
 
+    
+    @task()
+    def display_last_message():
+        print("---  SUCCESS: Ending this workflow🏁🚨 ---")
 
     # Define task dependencies to ensure the messages are displayed in order
+    starting_message = display_init_message()
     first_message = display_first_message()
     second_message = display_second_message()
     third_message = display_third_message()
+    ending_message = display_last_message()
 
-    first_message >> second_message >> third_message  # Set task execution order
+    starting_message >> first_message >> second_message >> third_message >> ending_message # Set task execution order
 
 message_display_pipeline = message_display_dag()
